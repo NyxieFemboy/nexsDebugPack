aa6bf0bc1db038d056bcf009b9f3d15c72702653
NyxieFemboy <therealnexlevel@gmail.com>
2024-09-14 15:44:23 -0400

    some changes to make things a lil more optimized

--- data/stages/testStage.hx
+++ data/stages/testStage.hx
@@ -1,5 +1,6 @@
 //dont remove this
 import flixel.addons.display.FlxBackdrop;
+import flixel.util.FlxSort;
 
 function create()
 {
@@ -20,31 +21,22 @@ function create()
     insert(2, platform);
     
     bfPlaceHolder = new FlxSprite(1075, 450);
-    bfPlaceHolder.frames = Paths.getSparrowAtlas('characters/bf');
+    bfPlaceHolder.frames = Paths.getSparrowAtlas('characters/bfJustIdle');
     bfPlaceHolder.animation.addByPrefix('idle', 'BF idle dance', 24, false);
     bfPlaceHolder.animation.play("idle");
-    bfPlaceHolder.antialiasing = false;
-    bfPlaceHolder.alpha = 0.5;
-    bfPlaceHolder.colorTransform.color = FlxColor.GRAY;
     insert(3, bfPlaceHolder);
 
     dadPlaceHolder = new FlxSprite(100 - 300, 100);
-    dadPlaceHolder.frames = Paths.getSparrowAtlas('characters/dad');
+    dadPlaceHolder.frames = Paths.getSparrowAtlas('characters/dadJustIdle');
     dadPlaceHolder.animation.addByPrefix('idle', 'Dad idle dance', 24, false);
     dadPlaceHolder.animation.play("idle");
-    dadPlaceHolder.antialiasing = false;
-    dadPlaceHolder.alpha = 0.5;
-    dadPlaceHolder.colorTransform.color = FlxColor.GRAY;
     dadPlaceHolder.flipX = false;
     insert(4, dadPlaceHolder);
 
     gfPlaceHolder = new FlxSprite(351, 172);
-    gfPlaceHolder.frames = Paths.getSparrowAtlas('characters/gf');
+    gfPlaceHolder.frames = Paths.getSparrowAtlas('characters/gfJustIdle');
     gfPlaceHolder.animation.addByPrefix('idle', 'GF Dancing Beat0', 24, false);
     gfPlaceHolder.animation.play("idle");
-    gfPlaceHolder.antialiasing = false;
-    gfPlaceHolder.alpha = 0.5;
-    gfPlaceHolder.colorTransform.color = FlxColor.GRAY;
     gfPlaceHolder.flipX = false;
     insert(5, gfPlaceHolder);
 
@@ -79,6 +71,13 @@ function postCreate()
         strumLines.members[1].characters[i].x = boyfriend.x;
         strumLines.members[1].characters[i].y = boyfriend.y;
     }
+
+    for (i in 0...strumLines.members[0].characters.length) {
+        strumLines.members[0].characters[i].x = dad.x + (50 * i);
+        strumLines.members[0].characters[i].y = dad.y - (50 * i);
+    }
+
+    strumLines.members[0].characters.sort(FlxSort.byY, FlxSort.ASCENDING);
 }
 
 function beatHit()
